399fe8d4634fa498df14fae2170a856749c16254
zesciati <zakyabiyyu2003@gmail.com>
2025-07-26 00:29:18 +0800

    memahami classlist untuk memunculkan dan menghilangkan konten

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const submitButtonEl = document.querySelector(".submit-button");
 const errorMsg = document.querySelector(".error-msg");
+const ratingContainerEl = document.querySelector(".rating-container");
+const thankContainerEl = document.querySelector(".thank-container")
 
+let ratingValue = null;
+submitButtonEl.addEventListener("click", function()  {
+  if(ratingValue){
+    // jika ratingValue = true maka ratingContainer akan hilang dengan menambahkan kelas css hidden dan remove kelas hidden untuk thankContainerEl
+    ratingContainerEl.classList.add("hidden");
+    thankContainerEl.classList.remove("hidden");
+  }else{
 // cara menampilkan error message dengan submit button
-// Buat fungsi tanpa parameter, lalu jalankan kode ini saat tombol diklik.
-submitButtonEl.addEventListener("click", () => {
-  errorMsg.textContent = "halo";
+  errorMsg.textContent = "Please choose the rating number";
+  }
 }
   
 
