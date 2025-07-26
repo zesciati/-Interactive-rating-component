15af72e363a6c48d1885cec117a8ebc502088b22
zesciati <zakyabiyyu2003@gmail.com>
2025-07-27 01:45:25 +0800

    little challenge that I made for myself half of it coming from me the rest from gpt kinda  broke my heart

--- README.md
+++ README.md
@@ -7,7 +7,9 @@ npm run dev
 The website was developed with a mobile size, namely a viewport of 375px -> then developed to a desktop size, namely a viewport of 1440px
 
 ### js plan
-After done all the process I want to coming back to rating state(brand new)
+After done all the process I want to coming back to rating state(brand new):
+- [x] adding button in thank you state
+- [ ]  
 
 ### other thing
 /* NOTE if you want to edit with regular css do it in input.css  */
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const submitButtonEl = document.querySelector(".submit-button");
+const submitButtonThank = document.querySelector(".submit-button-thank");
 const errorMsg = document.querySelector(".error-msg");
 const ratingContainerEl = document.querySelector(".rating-container");
 const thankContainerEl = document.querySelector(".thank-container");
@@ -10,6 +11,7 @@ let ratingValue = null;
 
 // Why doesn't rateButtonsEl directly use addEventListener? Because it needs to use foreach first to check each element that has a rateButtonsEl value.
 
+/* -------- Process to get value button to display in thank you state ------- */
 // rateButton represents rateButtonsEl
 rateButtonsEl.forEach(function(rateButton){
   // If the user clicks on a certain value, the value will be retrieved with getAttribute and then displayed with selectedEl 
@@ -20,24 +22,31 @@ rateButtonsEl.forEach(function(rateButton){
       
       if(ratingValue){
         errorMsg.textContent  = "";
+        // delete ratingValue;
       }
   })
 })
 
-
-
-
-
+/* ------------------------------ Submit button ----------------------------- */
 submitButtonEl.addEventListener("click", function()  {
-  if(ratingValue){
-    // if ratingValue = true then ratingContainer will disappear by adding hidden css class and remove hidden class for thankContainerEl
-    ratingContainerEl.classList.add("hidden");
-    thankContainerEl.classList.remove("hidden");
-  }else{
-    // how to display error message with submit button
-  errorMsg.textContent = "Please choose the rating number";
+    if(ratingValue){
+      // if ratingValue = true then ratingContainer will disappear by adding hidden css class and remove hidden class for thankContainerEl
+      ratingContainerEl.classList.add("hidden");
+      thankContainerEl.classList.remove("hidden");
+    }else{
+      // how to display error message with submit button
+    errorMsg.textContent = "Please choose the rating number";
+    }
   }
-}
-  
-
 )
+
+/* ----------------------------- back to button ----------------------------- */
+function backToRate(){
+  thankContainerEl.classList.add("hidden");
+  ratingContainerEl.classList.remove("hidden");
+  ratingValue = null;
+  selectedEl.textContent = "";
+}
+
+submitButtonThank.addEventListener("click",backToRate);
+
