83afff14368c92e55c550bed74c6c0972e69d9a0
zesciati <zakyabiyyu2003@gmail.com>
2025-07-26 23:51:29 +0800

    selesai js tapi challenge menambahkan btn Back di thank you state

--- README.md
+++ README.md
@@ -6,6 +6,9 @@ npm run dev
 ### layout plan:
 The website was developed with a mobile size, namely a viewport of 375px -> then developed to a desktop size, namely a viewport of 1440px
 
+### js plan
+After done all the process I want to coming back to rating state(brand new)
+
 ### other thing
 /* NOTE if you want to edit with regular css do it in input.css  */
 
--- index.js
+++ index.js
@@ -1,16 +1,40 @@
 const submitButtonEl = document.querySelector(".submit-button");
 const errorMsg = document.querySelector(".error-msg");
 const ratingContainerEl = document.querySelector(".rating-container");
-const thankContainerEl = document.querySelector(".thank-container")
+const thankContainerEl = document.querySelector(".thank-container");
+const rateButtonsEl = document.querySelectorAll(".rate-button");
+const selectedEl = document.querySelector(".selected");
 
 let ratingValue = null;
+
+
+// Why doesn't rateButtonsEl directly use addEventListener? Because it needs to use foreach first to check each element that has a rateButtonsEl value.
+
+// rateButton represents rateButtonsEl
+rateButtonsEl.forEach(function(rateButton){
+  // If the user clicks on a certain value, the value will be retrieved with getAttribute and then displayed with selectedEl 
+  rateButton.addEventListener("click", function(){
+      ratingValue = rateButton.getAttribute("value");
+
+      selectedEl.textContent = `You selected ${ratingValue} out 5` ;
+      
+      if(ratingValue){
+        errorMsg.textContent  = "";
+      }
+  })
+})
+
+
+
+
+
 submitButtonEl.addEventListener("click", function()  {
   if(ratingValue){
-    // jika ratingValue = true maka ratingContainer akan hilang dengan menambahkan kelas css hidden dan remove kelas hidden untuk thankContainerEl
+    // if ratingValue = true then ratingContainer will disappear by adding hidden css class and remove hidden class for thankContainerEl
     ratingContainerEl.classList.add("hidden");
     thankContainerEl.classList.remove("hidden");
   }else{
-// cara menampilkan error message dengan submit button
+    // how to display error message with submit button
   errorMsg.textContent = "Please choose the rating number";
   }
 }
